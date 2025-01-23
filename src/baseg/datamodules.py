379454792f7f693683f15e3b09cc4f735b3265ca
Luca Barco <luca.barco@linksfoundation.com>
2025-01-23 21:59:59 +0100

    update datasets

--- src/baseg/datamodules.py
+++ src/baseg/datamodules.py
@@ -5,7 +5,7 @@ from albumentations.pytorch import ToTensorV2
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader
 
-from baseg.datasets import EMSCropDataset, EMSImageDataset
+from baseg.datasets import EMSCropDataset, EMSImageDataset, EFFISCropDataset, EFFISImageDataset
 from baseg.samplers import SequentialTiledSampler
 from baseg.samplers.single import RandomTiledSampler
 
@@ -58,12 +58,14 @@ class EMSDataModule(LightningDataModule):
                 subset="train",
                 modalities=self.modalities,
                 transform=self.train_transform,
+                gt_label="DEL" if "DEL" in self.modalities else "GRA",
             )
             self.val_set = EMSCropDataset(
                 root=self.root,
                 subset="val",
                 modalities=self.modalities,
                 transform=self.eval_transform,
+                gt_label="DEL" if "DEL" in self.modalities else "GRA",
             )
         elif stage == "test":
             self.test_set = EMSCropDataset(
@@ -71,6 +73,7 @@ class EMSDataModule(LightningDataModule):
                 subset="test",
                 modalities=self.modalities,
                 transform=self.eval_transform,
+                gt_label="DEL" if "DEL" in self.modalities else "GRA",
             )
         elif stage == "predict":
             self.pred_set = EMSImageDataset(
@@ -78,6 +81,121 @@ class EMSDataModule(LightningDataModule):
                 subset="test",
                 modalities=self.modalities,
                 transform=self.eval_transform,
+                gt_label="DEL" if "DEL" in self.modalities else "GRA",
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_set,
+            sampler=RandomTiledSampler(self.train_set, tile_size=self.patch_size),
+            batch_size=self.batch_size_train,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_set,
+            sampler=SequentialTiledSampler(
+                self.val_set,
+                tile_size=self.patch_size,
+            ),
+            batch_size=self.batch_size_eval,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_set,
+            sampler=SequentialTiledSampler(
+                self.test_set,
+                tile_size=self.patch_size,
+            ),
+            batch_size=self.batch_size_eval,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.pred_set,
+            shuffle=False,
+            batch_size=1,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+class EFFISDataModule(LightningDataModule):
+    transform_targets = {
+        "post": "image",
+        "mask": "mask",
+        "dNBR": "mask",
+    }
+
+    def __init__(
+        self,
+        root: Path,
+        patch_size: int = 512,
+        modalities: list[str] = ["post", "mask", "dNBR"],
+        batch_size_train: int = 8,
+        batch_size_eval: int = 16,
+        num_workers: int = 4,
+    ) -> None:
+        super().__init__()
+        self.root = root
+        self.modalities = modalities
+        self.patch_size = patch_size
+        self.batch_size_train = batch_size_train
+        self.batch_size_eval = batch_size_eval
+        self.num_workers = num_workers
+        self.train_transform = A.Compose(
+            [
+                A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.5),
+                A.RandomRotate90(p=0.5),
+                A.ShiftScaleRotate(rotate_limit=360, value=0, mask_value=255, p=0.5),
+                A.RandomBrightnessContrast(p=0.5, brightness_limit=0.02, contrast_limit=0.02),
+                ToTensorV2(),
+            ],
+            additional_targets=self.transform_targets,
+        )
+        self.eval_transform = A.Compose(
+            [ToTensorV2()],
+            additional_targets=self.transform_targets,
+        )
+
+    def setup(self, stage=None):
+        if stage == "fit":
+            self.train_set = EFFISCropDataset(
+                root=self.root,
+                subset="train",
+                modalities=self.modalities,
+                transform=self.train_transform,
+                gt_label="mask" if "mask" in self.modalities else "dNBR",
+            )
+            self.val_set = EFFISCropDataset(
+                root=self.root,
+                subset="val",
+                modalities=self.modalities,
+                transform=self.eval_transform,
+                gt_label="mask" if "mask" in self.modalities else "dNBR",
+            )
+        elif stage == "test":
+            self.test_set = EFFISCropDataset(
+                root=self.root,
+                subset="test",
+                modalities=self.modalities,
+                transform=self.eval_transform,
+                gt_label="mask" if "mask" in self.modalities else "dNBR",
+            )
+        elif stage == "predict":
+            self.pred_set = EFFISImageDataset(
+                root=self.root,
+                subset="test",
+                modalities=self.modalities,
+                transform=self.eval_transform,
+                gt_label="mask" if "mask" in self.modalities else "dNBR",
             )
 
     def train_dataloader(self):
--- src/baseg/datasets.py
+++ src/baseg/datasets.py
@@ -87,6 +87,7 @@ class EMSImageDataset(Dataset):
         modalities: list[str] = ["S2L2A", "DEL", "ESA_LC", "CM"],
         transform: Callable = None,
         check_integrity: bool = True,
+        gt_label: str = "DEL"
     ):
         self.root = Path(root)
         modalities = set(modalities)
@@ -98,11 +99,13 @@ class EMSImageDataset(Dataset):
         self.modalities = modalities
         self.transform = transform
         self.files = {}
+        self.gt_label = gt_label
         # gather all the files
         for modality in self.modalities:
             rasters = sorted((self.root / subset).glob(f"**/*_{modality}.tif"))
             assert len(rasters) > 0, f"no rasters found for modality {modality}"
             self.files[modality] = rasters
+            print(f"Modality: {modality}, Files: {len(rasters)}")
         # filter out the files that are not present in all modalities, make sure file names match
         self.files = self._filter_files(self.files)
         self.activations = [d for d in (self.root / subset).iterdir() if d.is_dir()]
@@ -114,6 +117,7 @@ class EMSImageDataset(Dataset):
         """Get the stem, minus the modality.
         From file names like EMSR456_AOI21_20_01_ESA_LC.tif, get EMSR456_AOI21_20_01.
         """
+        # print(file_path)
         return "_".join(file_path.stem.split("_", maxsplit=4)[:3])
 
     def _filter_files(self, files: dict[str, list[Path]]) -> dict[str, list[Path]]:
@@ -163,7 +167,7 @@ class EMSImageDataset(Dataset):
 
     def _preprocess(self, sample: dict) -> dict:
         sample["image"] = np.clip(sample.pop("S2L2A").transpose(1, 2, 0), 0, 1)
-        mask = sample.pop("DEL")
+        mask = sample.pop(self.gt_label)
         # if we have a cloud mask, use it to ignore pixels in DEL and LC
         if "CM" in sample:
             cm = sample.pop("CM")
@@ -175,7 +179,7 @@ class EMSImageDataset(Dataset):
 
     def _postprocess(self, sample: dict) -> dict:
         sample["S2L2A"] = sample.pop("image")
-        sample["DEL"] = sample.pop("mask")
+        sample[self.gt_label] = sample.pop("mask")
         return sample
 
     def __len__(self) -> int:
@@ -214,5 +218,182 @@ class EMSCropDataset(EMSImageDataset):
             sample = self._postprocess(self.transform(**self._preprocess(sample)))
         sample["metadata"] = metadata
         return sample
+        sample["metadata"] = metadata
+        return sample
+
+
+
+class EFFISImageDataset(Dataset):
+    """Dataset using CEMS data. The format is the following:
+
+    root
+    ├── 45662
+    │   ├── mask
+    |   |   ├── mask.tiff   
+    │   ├── post
+    |   |   ├── map.tiff    
+    │   ├── pre
+    |   |   ├── map.tiff
+    |
+    ├── ...
+
+    """
+    bands = {
+        "pre": None,
+        "post": None,
+        "mask": 1,
+        "dNBR": 1
+    }
+    modalities_files = {
+        "pre": "map",
+        "post": "map",
+        "mask": "mask",
+        "dNBR": "dNBR_categorized"
+    }
+    dtypes = {
+        "pre": "float32",
+        "post": "float32",
+        "mask": "uint8",
+        "dNBR": "float32",
+    }
+
+    def __init__(
+        self,
+        root: Path,
+        subset: str,
+        modalities: list[str] = ["pre", "post", "mask", "dNBR"],
+        transform: Callable = None,
+        check_integrity: bool = True,
+        gt_label: str = "mask",
+        cloud_masking: bool = False
+    ):
+        self.root = Path(root)
+        self.cloud_masking = cloud_masking
+        modalities = set(modalities)
+        assert subset in ["train", "val", "test"], f"subset must be one of train, val, test, got {subset}"
+        assert self.root.exists(), f"root {self.root} does not exist"
+        assert all(modality in ["pre", "post", "mask", "dNBR", "dNBR"] for modality in modalities)
+        assert len(modalities) > 0, "modalities must not be empty"
+        assert "post" in modalities, "At least S2L2A must be present in the modalities"
+        self.modalities = modalities
+        self.transform = transform
+        self.files = {}
+        self.gt_label = gt_label
+        # gather all the files
+        for modality in self.modalities:
+            rasters = sorted((self.root / subset).glob(f"**/{modality}/{self.modalities_files[modality]}.tiff"))
+            assert len(rasters) > 0, f"no rasters found for modality {modality}"
+            self.files[modality] = rasters
+        # filter out the files that are not present in all modalities, make sure file names match
+        self.files = self._filter_files(self.files)
+        self.activations = [d for d in (self.root / subset).iterdir() if d.is_dir()]
+        # double check that the file IDs match at the same index across modalities
+        if check_integrity:
+            self._check_integrity()
+
+    def _file_id(self, file_path: Path) -> str:
+        """Get the stem, minus the modality.
+        From file names like EMSR456_AOI21_20_01_ESA_LC.tif, get EMSR456_AOI21_20_01.
+        """
+        return str(file_path).split("/", maxsplit=4)[-3]
+
+    def _filter_files(self, files: dict[str, list[Path]]) -> dict[str, list[Path]]:
+        """Filter out the files that are not present in all modalities, using the file IDs."""
+        # get the file IDs that are present in all modalities
+        intersection = set()
+        for modality, modality_files in files.items():
+            modality_ids = set(self._file_id(file_path) for file_path in modality_files)
+
+            # compute the intersection of the file IDs
+            if len(intersection) == 0:
+                intersection = modality_ids
+            else:
+                intersection = intersection.intersection(modality_ids)
+        assert len(intersection) > 0, "no common file IDs found"
+
+        # filter out the modalities that do not have the same file IDs
+        filtered = {}
+        for modality, modality_files in files.items():
+            filtered[modality] = sorted(
+                file_path for file_path in modality_files if self._file_id(file_path) in intersection
+            )
+            assert len(filtered[modality]) > 0, f"no files found for modality {modality}"
+
+        return filtered
+
+    def _check_integrity(self):
+        """Double check that the file IDs match at the same index across modalities."""
+        lengths = [len(modality_files) for modality_files in self.files.values()]
+        assert all(length == lengths[0] for length in lengths), "modalities have different number of files"
+        # zip the files together
+        zipped = zip(*self.files.values())
+        for idx, file_paths in enumerate(zipped):
+            # assert all file IDs match
+            file_ids = [self._file_id(file_path) for file_path in file_paths]
+            assert all(
+                file_id == file_ids[0] for file_id in file_ids
+            ), f"file IDs do not match at index {idx}: {file_ids}"
+
+    def image_shapes(self) -> list[tuple[int, int]]:
+        """Get the shapes of all the images in the dataset."""
+        shapes = []
+        for files in self.files["post"]:
+            profile = read_raster_profile(files)
+            shapes.append((profile["width"], profile["height"]))
+        return shapes
+
+    def _preprocess(self, sample: dict) -> dict:
+        sample["image"] = np.clip(sample.pop("post").transpose(1, 2, 0), 0, 1)
+        mask = sample.pop(self.gt_label)
+        # if we have a cloud mask, use it to ignore pixels in DEL and LC
+        if "CM" in sample:
+            cm = sample.pop("CM")
+            mask[cm == 1] = 255
+        sample["mask"] = mask
+        return sample
+
+    def _postprocess(self, sample: dict) -> dict:
+        sample["post"] = sample.pop("image")
+        sample[self.gt_label] = sample.pop("mask")
+        return sample
+
+    def __len__(self) -> int:
+        return len(self.files["post"])
+
+    def __getitem__(self, idx: int) -> dict:
+        sample = {}
+        metadata = dict(idx=idx)
+        for modality, modality_files in self.files.items():
+            file_path = modality_files[idx]
+            sample[modality] = read_raster(
+                file_path,
+                bands=self.bands[modality],
+            ).astype(self.dtypes[modality])
+            metadata[modality] = str(file_path)
+            if modality == "post" and self.cloud_masking:
+                file_path = modality_files[idx].replace("post", "cc")
+                sample["CM"] = read_raster(file_path, bands=1).astype("uint8")
+                metadata["CM"] = str(file_path)
+        if self.transform:
+            sample = self._postprocess(self.transform(**self._preprocess(sample)))
+        sample["metadata"] = metadata
+        return sample
+
+
+class EFFISCropDataset(EFFISImageDataset):
+    def __getitem__(self, bounds: IndexedBounds) -> dict:
+        idx, coords = bounds.index, bounds.coords
+        sample = {}
+        metadata = dict(idx=idx, coords=coords)
+        for modality, modality_files in self.files.items():
+            file_path = modality_files[idx]
+            sample[modality] = read_raster(
+                file_path,
+                bands=self.bands[modality],
+                window=Window(*coords),
+            ).astype(self.dtypes[modality])
+            metadata[modality] = str(file_path)
+        if self.transform:
+            sample = self._postprocess(self.transform(**self._preprocess(sample)))
         sample["metadata"] = metadata
         return sample
